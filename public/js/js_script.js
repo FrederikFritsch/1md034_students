44c154fd6048f312382cc83dc4d38f8bf04dbc71
FrederikFritsch <frederik1836@gmail.com>
2020-02-01 15:36:22 +0100

    Task 4-3 completed.

---
+++ public/js/js_script.js
@@ -0,0 +1,32 @@
+function menuItem(burger, kcal, all, ing, src) {
+    this.name = burger;
+    this.kcal = kcal;
+    this.allergies = all;
+    this.ingredients = ing;
+    this.image = src;    
+
+    this.info = function() {
+	return this.name + ' ' + this.kcal;
+    }
+}
+
+
+
+
+let FireBurger = new menuItem('FireBurger', '750 kCal', 'gluten and lactose', 'meat', 'https://image.shutterstock.com/image-photo/closeup-home-made-tasty-burger-600w-1291051606.jpg');
+
+let TurkeyBurger = new menuItem('TurkeyBurger', '850 kCal', 'lactose', 'turkey', 'https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/6:4/w_620%2Ch_413/shutterstock_148839410.jpg');
+
+let DoubleCheese = new menuItem('DoubleCheese', '950 kCal', 'gluten and lacose', 'meat', 'https://images.eatthismuch.com/site_media/img/331330_dylpill21_acbc0262-ff8f-4a8e-b6d0-25486109a502.png');
+
+let TrippleCheese = new menuItem('TrippleCheese', '1100 kCal', 'gluten and lacose', 'meat', 'https://images.eatthismuch.com/site_media/img/331330_dylpill21_acbc0262-ff8f-4a8e-b6d0-25486109a502.png');
+
+let QuadraCheese = new menuItem('QuadraCheese', '1250 kCal', 'gluten and lacose', 'meat', 'https://images.eatthismuch.com/site_media/img/331330_dylpill21_acbc0262-ff8f-4a8e-b6d0-25486109a502.png');
+
+
+console.log(FireBurger.info());
+console.log(TurkeyBurger.info());
+console.log(DoubleCheese.info());
+console.log(TrippleCheese.info());
+console.log(QuadraCheese.info());
+
