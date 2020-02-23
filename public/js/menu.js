f4ba6ed56b8ca2208b1a17b2225a9d0d5cf40c36
FrederikFritsch <frederik1836@gmail.com>
2020-02-23 13:19:44 +0100

    Supplementation: fixed dynamic Burgertable and removed hardcoding

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,5 +1,6 @@
 let food = [
     {
+	"id": "fireburger",
 	"name": "Fire Burger",
 	"kCal": "750 kCal",
 	"info1": "Contains lactose",
@@ -8,6 +9,7 @@ let food = [
     },
 
     {
+	"id": "turkeyburger",
 	"name": "Turkey Burger",
 	"kCal": "850 kCal",
 	"info1": "Deep fired Turkish turkey",
@@ -16,10 +18,27 @@ let food = [
     },
     
     {
+	"id": "doublecheese",
 	"name": "A double w/ Cheese",
 	"kCal": "950 kCal",
 	"info1": "Lots of yummy cheese",
 	"info2": "Sweet bread bun",
 	"img": "https://images.eatthismuch.com/site_media/img/331330_dylpill21_acbc0262-ff8f-4a8e-b6d0-25486109a502.png"
-    }
+    },
+    {
+	"id": "tripplecheese",
+	"name": "Tripple Cheese",
+	"kCal": "1050 kCal",
+	"info1": "Lots of cheese and meat",
+	"info2": "Contains lactose",
+	"img": "https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/6:4/w_620%2Ch_413/shutterstock_148839410.jpg"
+    },
+    {
+	"id": "quadracheese",
+	"name": "Quadra Cheese",
+	"kCal": "1200 kCal",
+	"info1": "Contains lactose",
+	"info2": "Meat",
+	"img": "https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/6:4/w_620%2Ch_413/shutterstock_148839410.jpg"
+    },
 ]
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -26,52 +26,17 @@
 
 
 window.onload = function () {
-
-    const vm0 = new Vue({
-	el: '#burger1',
+    const dynamicvm = new Vue ({
+	el:'#dynamicBurger',
 	data: {
-	    burger1Header: food[0].name,
-	    image1: food[0].img,
-	    burger1List: [
-		{burger1info: food[0].kCal},
-		{burger1info: food[0].info1},
-		{burger1info: food[0].info2}
-	    ]
-	    
-	}
-    });
-    const vm2 = new Vue({
-	el: '#burger2',
-	data: {
-	    burger2Header: "Turkey Burger",
-	    image2: "https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/6:4/w_620%2Ch_413/shutterstock_148839410.jpg",
-	    burger2List: [
-		{burger2info: food[1].kCal},
-		{burger2info: food[1].info1},
-		{burger2info: food[1].info2}
-	    ]
-	}
-    });
-    const vm3 = new Vue({
-	el: '#burger3',
-	data: {
-	    burger3Header: "A Double w/ Cheese",
-	    image3: "https://images.eatthismuch.com/site_media/img/331330_dylpill21_acbc0262-ff8f-4a8e-b6d0-25486109a502.png",
-	    burger3List: [
-		{burger3info: food[2].kCal},
-		{burger3info: food[2].info1},
-		{burger3info: food[2].info2}
-	    ]
-	}
+	    menu: food,
+	},
     });
 
     const vm35 = new Vue({
 	el:'.info',
 	data: {
 	    Gender:'',
-	    fullName:'',
-	    mail:'',
-	    Payment:'',
 	}
     });
     
@@ -83,46 +48,21 @@ window.onload = function () {
 	    mail:'',
 	    Payment:'',
 	    showGender:'',
-	    Burger1IsChecked:'',
-	    Burger2IsChecked:'',
-	    Burger3IsChecked:''
+	    checkedBurgers:'',
 	},
 	
 	methods: {
-	    markdown: function() {
+	    markdone: function() {
 		vm4.fullName = document.getElementById("fullname").value;
 		vm4.mail = document.getElementById("email").value;
-		//vm4.streetName = document.getElementById("street").value;
-		//vm4.houseNumber = document.getElementById("house").value;
 		vm4.Payment = document.getElementById("payment").value;
 		vm4.showGender = vm35.Gender;
 
-		if(document.getElementById("fireburger").checked)
-		{
-		    vm4.Burger1IsChecked = "Fire Burger";
-		}
-		else
-		{
-		    vm4.Burger1IsChecked = "";
-		}
-
-		if(document.getElementById("turkeyburger").checked)
-		{
-		    vm4.Burger2IsChecked = "Turkey Burger";
-		}
-		else
-		{
-		    vm4.Burger2IsChecked = "";
-		}
-
-		if(document.getElementById("doublecheese").checked)
-		{
-		    vm4.Burger3IsChecked = "Double Cheese";
-		}
-		else
-		{
-		    vm4.Burger3IsChecked = "";
-		}
+		for(items in food) {
+		    if(document.getElementById(food[items].id).checked) {
+			vm4.checkedBurgers += food[items].name + ' ';
+		    }
+		}	
 	    },
 	    addOrder: function() {
 		/* When you click in the map, a click event object is sent as parameter
@@ -137,7 +77,7 @@ window.onload = function () {
 			x: vm.localOrder.x,
 			y: vm.localOrder.y,
 		    },
-		    orderItems: [vm4.Burger1IsChecked + ' ' + vm4.Burger2IsChecked + ' ' + vm4.Burger3IsChecked],
+		    orderItems: [vm4.checkedBurgers],
 		    orderInfo: [vm4.fullName + ' (' + vm4.mail + ', ' + vm4.Payment + ', ' + vm4.showGender + ')'],
 		});
 	    },
